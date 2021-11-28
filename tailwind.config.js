439ad8583de61242c630a57dd3804e59a47b432d
Tomás Aguiar <aguiarhcode@gmail.com>
2021-11-28 00:03:02 +0000

    Packages

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        bgBlue: {
+          DEFAULT: '#0F1017',
+        },
+        textColor: {
+          light: '#DBDBDB',
+          DEFAULT: '#FFFFFF',
+          gray: '#AEAEAE',
+          dark: '#BEBAF5',
+        },
+        card:{
+          DEFAULT: '#232732',
+        },
+      },
+      width: {
+        '200': '700px',
+        '800': '1230px',
+      },
+      height: {
+        '1': '1px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
