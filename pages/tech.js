21985236032a1644496ad519bde1dcc885dfdba3
Tomás Aguiar <aguiarhcode@gmail.com>
2021-12-09 08:49:06 +0000

    preparing to deploy

---
+++ pages/tech.js
@@ -0,0 +1,14 @@
+export default function tech() {
+
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec euismod, nisi vel consectetur euismod,
+        urna nisl aliquam eros, eu consectetur nunc nisi
+        vitae nisl.
+      </p>
+    </div>
+  );
+}
